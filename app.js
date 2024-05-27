cf1f97673aaaac642a85d8bcd877a7c0df804a9c
Juan Morena <thewolfmodzyt@gmail.com>
2024-05-26 22:27:35 -0300

    Working starting time input, and validating function to prevent NaN values and negative numbers.

--- app.js
+++ app.js
@@ -22,9 +22,13 @@ const timerHandler = () => {
         startButton.innerText = "STOP";
         startButton.style.color = "#ec7676";
         timer = setInterval(() => {
-            timeRemaining--;
-            remaining.innerText = formatTime(timeRemaining);
-            if (timeRemaining == 0) clearInterval(timeRemaining);
+            if (timeRemaining == 0) {
+                clearInterval(timeRemaining);
+                timerHandler();
+            } else {
+                timeRemaining--;
+                remaining.innerText = formatTime(timeRemaining);
+            }
         }, 1000);
     } else {
         startButton.innerText = "START";
@@ -46,4 +50,32 @@ const formatTime = (time) => {
 
     minutes = minutes < 10 ? `0${minutes}` : minutes;
     return `${hours}:${minutes}:${seconds}`;
+};
+
+const changeStartTime = () => {
+    let seconds = document.getElementById("seconds");
+    let minutes = document.getElementById("minutes");
+    let hours = document.getElementById("hours");
+
+    if (seconds.value >= 60) seconds.value = 59;
+    else if (seconds.value < 0) seconds.value = 0;
+    else if (seconds.value == "") seconds.value = 0;
+
+    if (minutes.value >= 60) minutes.value = 59;
+    else if (minutes.value < 0) minutes.value = 0;
+    else if (minutes.value == "") minutes.value = 0;
+
+    if (hours.value >= 24) hours.value = 23;
+    else if (hours.value < 0) hours.value = 0;
+    else if (hours.value == "") hours.value = 0;
+
+    seconds = parseInt(seconds.value);
+    minutes = parseInt(minutes.value);
+    hours = parseInt(hours.value);
+
+    minutes.value = minutes.value >= 60 ? 59 : minutes.value;
+    hours.value = hours.value >= 24 ? 23 : hours.value;
+
+    timeRemaining = seconds + minutes * 60 + hours * 60 * 60;
+    remaining.innerText = formatTime(timeRemaining);
 };
